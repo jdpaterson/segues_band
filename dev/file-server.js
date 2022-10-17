281f24c1084d0c492e24b10fbc64ecbc5714f88b
J. Paterson <jaydpaterson@gmail.com>
2022-10-16 20:20:46 -0700

    instagram feed stuff

---
+++ dev/file-server.js
@@ -0,0 +1,15 @@
+var fs = require('fs'),
+    http = require('http');
+
+http.createServer(function (req, res) {
+  fs.readFile('./index.html', function (err,data) {
+    console.log(err);
+    if (err) {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+      return;
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
+}).listen(8080);
